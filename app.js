03c2c9fec500097da20d1e3a9e8c767314379aab
Max-Antoine Clément <67877770+maclementED@users.noreply.github.com>
2021-04-23 01:04:49 -0400

    fix

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const discord = require('discord.js');
 const client = new discord.Client();
 let conn;
-
+let isPlaying = false;
 client.once('ready', () => {
     console.log('Bot Online')
 })
 
 client.on("voiceStateUpdate", async(oldVoiceState, newVoiceState) => {
-    const fs = require('fs');
-
-    if (newVoiceState.channel) {
+    
+    if (newVoiceState.channel && !isPlaying) {
         if (newVoiceState.member.voice.channel.name === "Chill Lounge") {
             conn = await newVoiceState.member.voice.channel.join();
         }
 
         while (conn != undefined) {
+            isPlaying = true; 
             const dispatcher = conn.play("animal_crossing.mp3")
             
             await new Promise(r => setTimeout(r, 100*60*1000));
@@ -29,4 +29,4 @@ client.on("voiceStateUpdate", async(oldVoiceState, newVoiceState) => {
     };
 });
 
-client.login("Njk5Mzc1MzU5NTY4NzczMTIw.XpTeDw.uY7XlcZURxzcpvwL21Hf3FLQwuY");
+client.login("Njk5Mzc1MzU5NTY4NzczMTIw.XpTeDw.3CVh54PJLA73IxFL-4gG3g7A0aQ");
